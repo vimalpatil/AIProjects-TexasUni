6001a56c2cb812449088ffff3e51688c5f94e558
vimal patil <vimalpatil287@gmail.com>
2026-08-29 09:53:45 -0700

    complete  the first 3 exercises

--- misc.js
+++ misc.js
@@ -3,16 +3,27 @@
 // Exercise 1: Comment-Driven Development
 // Function to calculate the factorial of a number
 
-
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}           
 // Exercise 2: Array Operations
 const numbers = [1, 2, 3, 4, 5];
 
 // Filter even numbers
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+
 
 
 // Exercise 3: Function from Signature
 // Type this on a new line and press Enter:
 // function reverseString(str)
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
 
+    
 
 // Exercise 4: Process User Data 
